7c9852d9ac19a371c23b76ef931c1bee05f94419
Voloschuk08 <voloschuk07olia@gmail.com>
2024-10-23 00:00:38 +0300

    Add files via upload

---
+++ PR7.1/PR7.1/PR7.1.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void Create(int** T, const int rowCount, const int colCount, const int Low,const int High);
+void Print(int** T, const int rowCount, const int colCount);
+void Sort(int** T, const int rowCount, const int colCount);
+void Change(int** T, const int row1, const int row2, const int colCount);
+void Calc(int** T, const int rowCount, const int colCount, int& S, int& k);
+int main()
+{
+	srand((unsigned)time(NULL));
+	int Low = -17;
+	int High = 14;
+	int rowCount = 8;
+	int colCount = 5;
+
+	int** T = new int* [rowCount];
+	for (int i = 0; i < rowCount; i++)
+		T[i] = new int[colCount];
+
+	Create(T, rowCount, colCount, Low, High);
+	Print(T, rowCount, colCount);
+	Sort(T, rowCount, colCount);
+	Print(T, rowCount, colCount);
+
+	int S = 0;
+	int k = 0;
+	Calc(T, rowCount, colCount, S, k);
+	cout << "S = " << S << endl;
+	cout << "k = " << k << endl;
+
+	Print(T, rowCount, colCount);
+	for (int i = 0; i < rowCount; i++)
+		delete[] T[i];
+	delete[] T;
+	return 0;
+}
+void Create(int** T, const int rowCount, const int colCount, const int Low,const int High)
+{
+	for (int i = 0; i < rowCount; i++)
+		for (int j = 0; j < colCount; j++)
+			T[i][j] = Low + rand() % (High - Low + 1);
+}
+void Print(int** T, const int rowCount, const int colCount)
+{
+	cout << endl;
+	for (int i = 0; i < rowCount; i++)
+	{
+		for (int j = 0; j < colCount; j++)
+			cout << setw(4) << T[i][j];
+		cout << endl;
+	}
+	cout << endl;
+}
+void Sort(int** T, const int rowCount, const int colCount)
+{
+	for (int i0 = 0; i0 < rowCount - 1; i0++)
+		for (int i1 = 0; i1 < rowCount - i0 - 1; i1++)
+			if ((T[i1][0] > T[i1 + 1][0])
+				// Якщо перші стовпці однакові, сортуємо за другим
+				|| 
+				(T[i1][0] == T[i1 + 1][0] && 
+				 T[i1][1] > T[i1 + 1][1])
+				// Якщо перший і другий однакові, сортуємо за третім
+				|| (T[i1][0] == T[i1 + 1][0] && 
+					T[i1][1] == T[i1 + 1][1] && 
+					T[i1][2] > T[i1 + 1][2]))
+				Change(T, i1, i1 + 1, colCount);
+}
+void Change(int** T, const int row1, const int row2, const int colCount)
+{
+	int tmp;
+	for (int j = 0; j < colCount; j++)
+	{
+		tmp = T[row1][j];
+		T[row1][j] = T[row2][j];
+		T[row2][j] = tmp;
+	}
+}
+void Calc(int** T, const int rowCount, const int colCount, int& S, int& k)
+{
+	S = 0;
+	k = 0;
+	for (int i = 0; i < rowCount; i++)
+		for (int j = 0; j < colCount; j++)
+			if (T[i][j] < 0 && !(T[i][j] % 4 == 0))
+			{
+				S += T[i][j];
+				k++;
+				T[i][j] = 0;
+			}
+}
---
+++ PR7.1/UnitTest7.1/UnitTest7.1.cpp
@@ -0,0 +1,108 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR7.1/PR7.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest71
+{
+	TEST_CLASS(UnitTest71)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int rowCount = 8;
+			int colCount = 5;
+			int Low = -17;
+			int High = 14;
+
+			int** T = new int* [rowCount];
+			for (int i = 0; i < rowCount; i++)
+				T[i] = new int[colCount];
+
+			Create(T, rowCount, colCount, Low, High);
+
+			// Перевірка, чи всі елементи в межах [Low, High]
+			for (int i = 0; i < rowCount; i++)
+				for (int j = 0; j < colCount; j++)
+					Assert::IsTrue(T[i][j] >= Low && T[i][j] <= High, L"Element out of range!");
+
+			for (int i = 0; i < rowCount; i++)
+				delete[] T[i];
+			delete[] T;
+		}
+
+		// Тестування функції Sort
+		TEST_METHOD(TestSort)
+		{
+			int rowCount = 3;
+			int colCount = 5;
+
+			// Вихідна матриця для тестування сортування
+			int testMatrix[3][5] = {
+				{ 3, 2, 1, 0, 0 },
+				{ 2, 2, 1, 0, 0 },
+				{ 1, 2, 1, 0, 0 }
+			};
+
+			int** T = new int* [rowCount];
+			for (int i = 0; i < rowCount; i++)
+				T[i] = new int[colCount];
+
+			// Копіюємо дані у динамічну матрицю
+			for (int i = 0; i < rowCount; i++)
+				for (int j = 0; j < colCount; j++)
+					T[i][j] = testMatrix[i][j];
+
+			// Викликаємо функцію сортування
+			Sort(T, rowCount, colCount);
+
+			// Перевіряємо, чи відсортовано за першим, другим і третім стовпцями
+			Assert::IsTrue(T[0][0] <= T[1][0] && T[1][0] <= T[2][0], L"First column not sorted");
+			Assert::IsTrue(T[0][1] <= T[1][1] && T[1][1] <= T[2][1], L"Second column not sorted");
+			Assert::IsTrue(T[0][2] <= T[1][2] && T[1][2] <= T[2][2], L"Third column not sorted");
+
+			for (int i = 0; i < rowCount; i++)
+				delete[] T[i];
+			delete[] T;
+		}
+
+		// Тестування функції Calc
+		TEST_METHOD(TestCalc)
+		{
+			int rowCount = 3;
+			int colCount = 5;
+
+			// Вихідна матриця для тестування Calc
+			int testMatrix[3][5] = {
+				{ -5, -4, 12, -3, 7 },
+				{ -8, 3, 9, -2, 10 },
+				{ -7, 2, -4, 11, -6 }
+			};
+
+			int** T = new int* [rowCount];
+			for (int i = 0; i < rowCount; i++)
+				T[i] = new int[colCount];
+
+			// Копіюємо дані у динамічну матрицю
+			for (int i = 0; i < rowCount; i++)
+				for (int j = 0; j < colCount; j++)
+					T[i][j] = testMatrix[i][j];
+
+			int S = 0;
+			int k = 0;
+
+			// Викликаємо функцію Calc
+			Calc(T, rowCount, colCount, S, k);
+
+			// Перевіряємо суму та кількість
+			Assert::AreEqual(S, -23, L"Incorrect sum");
+			Assert::AreEqual(k, 5, L"Incorrect count of elements");
+
+			for (int i = 0; i < rowCount; i++)
+				delete[] T[i];
+			delete[] T;
+		}
+	};
+}
